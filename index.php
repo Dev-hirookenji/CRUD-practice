b55f3e7e6f2ce04e73361144520b4a7aa65377ea
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-11-30 20:35:04 +0800

    crud test

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    include "connect.php"
+    if(isset($_POST["submit"])){
+        $name=$_POST["name"];
+        $email=$_POST["email"];
+        $number=$_POST["mobile"];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,22 @@
     <title>CRUD Practice</title>
 </head>
 <body>
-    
+    <div class="container mx-auto pt-16" >
+        <div class="grid grid-flow-row justify-center space-y-1">
+            <form action="post">
+                <label class="pr-3">Name</label>
+                <input type="text" class="form-control w-52" id="name" placeholder="Enter your name" required class="border-gray-800">
+            </form>
+            <form action="post">
+                <label class="pr-3">Email</label>
+                <input type="email" class="form-control w-52" id="email" placeholder="Enter your email" required class="border-gray-800">
+            </form>
+            <form action="post">
+                <label class="pr-3">Phone Number</label>
+                <input type="number" class="form-control w-52" id="number" placeholder="Enter your Phone number" required class="border-gray-800">   
+            </form>
+            <button type="submit" class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 w-20 rounded" name="submit">Submit</button> 
+        </div> 
+    </div>
 </body>
 </html>
